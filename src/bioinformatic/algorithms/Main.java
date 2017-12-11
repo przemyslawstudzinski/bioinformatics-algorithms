fdcf3844f0597529c1b30b5ca0b019c57d29a7f0
przstudzinski <przemyslawstudzinski1993@gmail.com>
2017-12-11 22:28:15 +0100

    Fix files extension

--- src/bioinformatic/algorithms/Main.java
+++ src/bioinformatic/algorithms/Main.java
@@ -3,17 +3,17 @@ package bioinformatic.algorithms;
 public class Main {
 
     public static void main(String[] args) {
-        FileReader seq1 = new FileReader("Sequence1");
+        FileReader seq1 = new FileReader("Sequence1.txt");
         seq1.readFile();
         System.out.println(seq1.getContent());
         Sequence sequence1 = new Sequence(seq1.getContent());
 
-        FileReader seq2 = new FileReader("Sequence2");
+        FileReader seq2 = new FileReader("Sequence2.txt");
         seq2.readFile();
         System.out.println(seq2.getContent());
         Sequence sequence2 = new Sequence(seq2.getContent());
 
-        FileReader seq3 = new FileReader("similarity_matrix");
+        FileReader seq3 = new FileReader("similarity_matrix.txt");
         seq3.readFile();
         System.out.println(seq3.getContent());
         SimilarityMatrix similarityMatrix = new SimilarityMatrix();
